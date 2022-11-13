674504548a98e537c34475505bd2fff060848aba
Prysmytskyi-Oleksandr <san2003@ukr.net>
2022-11-13 23:29:52 +0200

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+
+const makeGalleryItems = (items) => {
+  
+    return items
+        .map(({ preview, original, description }) => {
+           return   `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      loading = "lazy"
+      class="gallery__image lazyload"
+      data-src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;  })
+        .join("");}
+galleryEl.insertAdjacentHTML("beforeend", makeGalleryItems(galleryItems));
+
+
+const onClick = (event) => {  
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')
+  ) { return; }
+  // if (event.target.nodeName !== 'IMG') { return; }
+  console.log(event.target.dataset.source);
+ 
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" >`)
+instance.show()
+  }
+galleryEl.addEventListener('click', onClick);
+
+
+
+// lazysizez
+
+if ('loading' in HTMLImageElement.prototype) {
+  console.log('Підтримує loading');
+  // const lazyimgs = document.querySelectorAll('img[loading = "lazy"]');
+  // lazyimgs.forEach(img => {
+  //   img.src = img.dataset.src;
+  // }); 
+}
+else{
+  console.log('Не підтримує loading');
+  const script = document.createElement('script');
+   script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+   script.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+   script.crossorigin = "anonymous";
+  
+  document.body.appendChild(script);
+
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryEl = document.querySelector('.gallery');
+
+const makeGalleryItems = (items) => {
+    return items.map(({ preview, original, description }) => {
+        return ` <a class="gallery__item" href="${original}">
+  <img class="gallery__image" loading="lazy" src="${preview}" alt="${description}" />
+</a>`;
+    })
+        .join("");
+} 
+
+galleryEl.insertAdjacentHTML("beforeend", makeGalleryItems(galleryItems));
 console.log(galleryItems);
+
+
+const onClickImage = (event) => {
+event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(event.target);
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionPosition: "bottom",
+        captionDelay: 250,
+        scrollZoom: false,
+    });
+}
+
+galleryEl.addEventListener('click', onClickImage);
+
+
